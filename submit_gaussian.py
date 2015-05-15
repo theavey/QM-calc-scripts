0f2f67e34f4d496fc801a214b52f8fb4e84b06ee
Thomas Heavey <thomasjheavey@gmail.com>
2015-05-15 14:28:30 -0400

    Changed submit Gau for batch processing
    submit_gaussian has been updated to allow for creating multiple
    submission scripts, and submitting all of them at once as well. Should
    still work fine with just one file as well.

--- submit_gaussian.py
+++ submit_gaussian.py
@@ -28,6 +28,8 @@
 import argparse   # For parsing commandline arguments
 import re         # RegEx package for sorting data
 import glob       # Allows referencing file system/file names
+import readline   # Allows easier file input (with tab completion?)
+import subprocess # Allows for submitting commands to the shell
 
 descrip = 'Create and submit a script to run a Gaussian job on SCC'
 
@@ -43,67 +45,110 @@ parser.add_argument('-t', '--time',
                     help='Time required as "hh:mm:ss"',
                     default='12:00:00')
 parser.add_argument('-b', '--batch', action='store_true',
-                    help='create multiple scripts (batch job)'
+                    help='create multiple scripts (batch job)')
 args = parser.parse_args()
 
-in_name = args.in_name
+# An input function that can prefill in the text entry
+def rlinput(prompt, prefill=''):
+   readline.set_startup_hook(lambda: readline.insert_text(prefill))
+   try:
+      return raw_input(prompt)
+   finally:
+      readline.set_startup_hook()
 
-if in_name.endswith('.com'):
-    short_name = re.split('\.', in_name)[0]
-    if not short_name + '.com' == in_name:
-        raise SyntaxError('problem interpretting file name. ' +
-                          'Period in file name?')
-    out_name = short_name + '.out'
-elif '.' in in_name:
-    short_name      = re.split('\.', in_name)[0]
-    input_extension = re.split('\.', in_name)[-1]
-    if not short_name + '.' + input_extension == in_name:
-        raise SyntaxError('problem interpretting file name. ' +\
-                          'Period in file name?')
-    out_name = short_name + '.out'
-else:
-    short_name = in_name
-    in_name    = short_name + '.com'
-    print('Assuming input file is {}'.format(in_name))
-    out_name   = short_name + '.out'
+in_name_list  =     glob.glob(args.in_name + '*')
+in_name_list.sort()        # sort files alphanumerically
+in_name_list.sort(key=len) # sort by length (because otherwise would
+# put 1,10,11,... as opposed to 1,...,9,10,...
+# if number 01,02,... They should all be the same length and the
+# second sort won't do anything.
 
-script_name = 'submit' + short_name + '.sh'
+num_files     = len(in_name_list)
 
+if not args.batch:
+    if num_files > 1:
+        print('Multiple files starting with {}'.format(args.in_name))
+        print('What file name shall I use?')
+        in_name_list = [ rlinput('file name: ',args.in_name)]
 
-with open(script_name, 'w') as script_file:
-    script_file.write('#!/bin/sh \n\n')
-    script_file.write('#$ -pe omp {}\n'.format(args.numcores))
-    script_file.write('#$ -M theavey@bu.edu\n')
-    script_file.write('#$ -m eas\n')
-    script_file.write('#$ -l h_rt={}\n'.format(args.time))
-    script_file.write('#$ -N {}\n'.format(short_name))
-    script_file.write('#$ -j y\n\n')
-    script_file.write('INPUTFILE={}\n'.format(in_name))
-    script_file.write('OUTPUTFILE={}\n\n'.format(out_name))
-    script_file.write('CURRENTDIR=`pwd`\n')
-    script_file.write('SCRATCHDIR=/scratch/$USER\n')
-    script_file.write('mkdir -p $SCRATCHDIR\n\n')
-    script_file.write('cd $SCRATCHDIR\n\n')
-    script_file.write('cp $CURRENTDIR/$INPUTFILE .\n\n')
-    script_file.write('g09 <$INPUTFILE > $OUTPUTFILE\n\n')
-    script_file.write('cp $OUTPUTFILE $CURRENTDIR/.\n\n')
-    script_file.write('echo ran in /net/`hostname -s`$SCRATCHDIR\n')
-    script_file.write('echo output was copied to $CURRENTDIR\n\n')
+script_list = []
 
-print('script written to {}'.format(script_name))
+for in_name in in_name_list:
+    if in_name.endswith('.com'):
+        short_name = re.split('\.', in_name)[0]
+        if not short_name + '.com' == in_name:
+            raise SyntaxError('problem interpretting file name. ' +
+                              'Period in file name?')
+        out_name = short_name + '.out'
+    elif '.' in in_name:
+        short_name      = re.split('\.', in_name)[0]
+        input_extension = re.split('\.', in_name)[-1]
+        if not short_name + '.' + input_extension == in_name:
+            raise SyntaxError('problem interpretting file name. ' +\
+                              'Period in file name?')
+        out_name = short_name + '.out'
+    else:
+        short_name = in_name
+        in_name    = short_name + '.com'
+        print('Assuming input file is {}'.format(in_name))
+        out_name   = short_name + '.out'
+
+    script_name = 'submit' + short_name + '.sh'
+    script_list.append(script_name)
+
+    with open(script_name, 'w') as script_file:
+        script_file.write('#!/bin/sh \n\n')
+        script_file.write('#$ -pe omp {}\n'.format(args.numcores))
+        script_file.write('#$ -M theavey@bu.edu\n')
+        script_file.write('#$ -m eas\n')
+        script_file.write('#$ -l h_rt={}\n'.format(args.time))
+        script_file.write('#$ -N {}\n'.format(short_name))
+        script_file.write('#$ -j y\n\n')
+        script_file.write('INPUTFILE={}\n'.format(in_name))
+        script_file.write('OUTPUTFILE={}\n\n'.format(out_name))
+        script_file.write('CURRENTDIR=`pwd`\n')
+        script_file.write('SCRATCHDIR=/scratch/$USER\n')
+        script_file.write('mkdir -p $SCRATCHDIR\n\n')
+        script_file.write('cd $SCRATCHDIR\n\n')
+        script_file.write('cp $CURRENTDIR/$INPUTFILE .\n\n')
+        script_file.write('g09 <$INPUTFILE > $OUTPUTFILE\n\n')
+        script_file.write('cp $OUTPUTFILE $CURRENTDIR/.\n\n')
+        script_file.write('echo ran in /net/`hostname -s`$SCRATCHDIR\n')
+        script_file.write('echo output was copied to $CURRENTDIR\n\n')
+
+    print('script written to {}'.format(script_name))
+
+if not len(script_list) == len(in_name_list):
+    # This should never be the case as far as I know, but I would
+    # like to make sure everything input gets a script and all the
+    # script names are there to be submitted.
+    raise IOError('num scripts dif. from num names given')
 
 yes = ['y', 'yes', '1']
 
-# Might eventually want to use readline package to help with
-# textual/file name input
-
-if input('submit job? ') in yes:
-    bashCommand = 'qsub {}'.format(script_name)
-    # Don't really know how this works. Copied from 
-    # http://stackoverflow.com/questions/4256107/
-    # running-bash-commands-in-python
-    import subprocess
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    print(output)
+if args.batch:
+    if input('submit all jobs? ') in yes:
+        for script in script_list:
+            bashCommand = 'qsub {}'.format(script)
+            # Don't really know how this works. Copied from 
+            # http://stackoverflow.com/questions/4256107/
+            # running-bash-commands-in-python
+            process = subprocess.Popen(bashCommand.split(),
+                                       stdout=subprocess.PIPE)
+            output = process.communicate()[0]
+            print(output)
+    else:
+        print('No jobs submitted, but scripts created')
+else:
+    if input('submit job {}? '.format(script_list[0])) in yes:
+        bashCommand = 'qsub {}'.format(script_list[0])
+        # Don't really know how this works. Copied from 
+        # http://stackoverflow.com/questions/4256107/
+        # running-bash-commands-in-python
+        process = subprocess.Popen(bashCommand.split(),
+                                   stdout=subprocess.PIPE)
+        output = process.communicate()[0]
+        print(output)
+    else:
+        print('{} not submitted'.format(script_list))
 
