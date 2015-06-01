fe84c9916a267b44274c2a3e0a04885b69cdf139
Thomas Heavey <thomasjheavey@gmail.com>
2015-06-01 17:06:52 -0400

    Mostly starting on processing single files with multiple geometries.

--- out_to_list.py
+++ out_to_list.py
@@ -54,11 +54,22 @@ descrip = ('This function takes a base file name for output files and'+
 parser = argparse.ArgumentParser(description=descrip)
 parser.add_argument('base_name', help='base name of files to read')
 parser.add_argument('-m', '--method',
-                    help='calculation method (changes file interp')
+                    help=('calculation method (changes how files '
+                          'are interpreted)'))
+parser.add_argument('-s', '--singlefile', action='store_true',
+                    help=('use if output is single file with '
+                         'multiple geometries'))
+parser.add_argument('-g', '--geometries', action='count', default=0,
+                    help=('Flag for creating file of geometries in '
+                    'XYZ style format.'
+                    '\n-g for stationary points, -gg for all.'))
+# todo add option for saving all energies -e?
+# maybe to separate file with different name? Probably not
+parser.add_argument('-a')
 args = parser.parse_args()
 
 base_name     =     args.base_name
-in_name_list  =     glob.glob(base_name + '*.out')
+in_name_list  =     glob.glob(base_name + '*out')
 num_files     = len(in_name_list)
 
 in_name_list.sort()        # sort files alphanumerically
@@ -67,9 +78,25 @@ in_name_list.sort(key=len) # sort by length (because otherwise would
 # if number 01,02,... They should all be the same length and the
 # second sort won't do anything.
 
-print('base file name is {}'.format(base_name))
-print('number of files is {:,}'.format(num_files))
-print('Energies of {}'.format(base_name))
+yes = ['y', 'yes', '1']
+
+if args.singlefile:
+    if num_files == 0:
+        in_name_list = glob.glob(base_name)[0]
+        num_files    = len(in_name_list)
+    if num_files == 1:
+        in_name = in_name_list[0]
+        print('(single) file name is {}'.format(in_name))
+    if num_files > 1:
+        print('More than one file matched input file name.')
+        if input('Get energies from multiple output files?') in yes:
+            args.singlefile = False
+        else:
+            raise SyntaxError('Okay, well files that '
+                  'matched your input are {}'.format(in_name_list))
+if not args.singlefile:
+    print('base file name is {}'.format(base_name))
+    print('number of files is {:,}'.format(num_files))
 
 
 out_name = base_name + '_energies'
@@ -91,7 +118,11 @@ i = 0
 method = 'nyd'
 # If given as commandline argument, set with the given method
 if args.method:
-    method = methods[args.method.lower()]
+    try:
+        method = methods[args.method.lower()]
+    except KeyError:
+        print("method {} unrecognized. Going to ".format(args.method)
+              + "try to find method based on output file.")
 
 # Gaussian method regex:
 # finds non-whitespace characters before a / and after a
@@ -99,7 +130,7 @@ if args.method:
 # (method)/(something, normally basis set).
 gregex = re.compile(r"\s+\S+/")
 # QChem SF methods energy regex:
-sfenergy = re.compile('[=:]\s*-\d*\.\d*')
+sfenergy = re.compile(r'[=:]\s*-\d*\.\d*')
 # Gaussian DFT methods ground state energy regex:
 # (could use sfenergy, or combine into one)
 gdftenergy = re.compile(r'=\s*-\d*\.\d*')
@@ -108,7 +139,11 @@ tdgdftenergy = re.compile(r'\s-*\d+\.\d+\s+ev')
 # Gaussian CIS ground state energy regex:
 # Goes to end of the line because given in scientific notation.
 gcisenergy = re.compile(r'eump2\s*=\s*-.+')
+# todo write geometry regex
+# todo find some way to find "stationary point" and process as needed
 
+
+# todo add geometry getting to all this multifile stuff
 with open(out_name, 'w') as out_file:
     for name in in_name_list:
         energy_list = []
@@ -207,13 +242,18 @@ with open(out_name, 'w') as out_file:
                         if match:
                             energy_list.append(match.group()[:-3])
                     continue
-        if energy_list:
+        # if energy_list:
             # Only true if not empty
+        if True:
+            # Using for now because some error blank files
+            # should still produce a line in the output, even if blank
             out_file.write(str(energy_list) + '\n')
         i += 1
+# todo save files with desired information
 
 print("Opened {0} files, and wrote data to {1}".format(i, out_name))
 print('Files processed for {} method.'.format(method))
+# todo write statements about files that were saved
 
 
                 
