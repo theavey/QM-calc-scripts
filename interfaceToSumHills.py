0e9b666809fc2b823abcbd1819841face9849585
Thomas Heavey <thomasjheavey@gmail.com>
2015-08-28 16:51:36 -0400

    Script to run PLUMED's sum_hills and collect results into a Mathematica readable format

---
+++ interfaceToSumHills.py
@@ -0,0 +1,269 @@
+#! /usr/bin/env python3.4
+
+########################################################################
+#                                                                      #
+# This script was written by Thomas Heavey in 2015.                    #
+#        theavey@bu.edu     thomasjheavey@gmail.com                    #
+#                                                                      #
+# Copyright 2015 Thomas J. Heavey IV                                   #
+#                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");      #
+# you may not use this file except in compliance with the License.     #
+# You may obtain a copy of the License at                              #
+#                                                                      #
+#    http://www.apache.org/licenses/LICENSE-2.0                        #
+#                                                                      #
+# Unless required by applicable law or agreed to in writing, software  #
+# distributed under the License is distributed on an "AS IS" BASIS,    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      #
+# implied.                                                             #
+# See the License for the specific language governing permissions and  #
+# limitations under the License.                                       #
+#                                                                      #
+########################################################################
+
+# This is written to work with python 3.4 because it should be good to
+# be working on the newest version of python.
+
+import glob
+import argparse
+import subprocess
+import readline
+import os
+import shutil
+from datetime import datetime
+
+parser = argparse.ArgumentParser(description='Use PLUMED utility to sum '
+                                             'HILLS and then put into '
+                                             'Mathematica friendly format')
+parser.add_argument('-H', '--hills', default='HILLS',
+                    help='name of the HILLS file')
+parser.add_argument('-s', '--stride', default=10000,
+                    help='specify the stride for integrating '
+                         'hills file (default 1000)')
+parser.add_argument('-i', '--min', default=None, # todo add type/''
+                    help='the lower bounds for the grid')
+parser.add_argument('-a', '--max', default=None,
+                    help='the upper bounds for the grid')
+parser.add_argument('-b', '--bin', default=None,
+                    help='the number of bins for the grid')
+parser.add_argument('-p', '--spacing', default=None,
+                    help='The spacing for the grid. Currently unused')
+parser.add_argument('-v', '--verbose', action='store_true',
+                    help='make script more verbose')
+parser.add_argument('-f', '--folder', default='SumHills',
+                    help='Folder in which this will be run')
+parser.add_argument('-t', '--temp_file', default='temp_data_file.m',
+                    help='File in which to store all the data')
+parser.add_argument('-n', '--var_name', default='summedHills',
+                    help='Name of variable to be assigned for Mathematica')
+parser.add_argument('-T', '--template', default='sumHillsTempl.m',
+                    help='Output template file')
+parser.add_argument('-o', '--output_name', default='summedHills.m',
+                    help='Name of the file to be output')
+parser.add_argument('-e', '--exists', action='store_true',
+                    help='Use this argument if the fes data already exists')
+args = parser.parse_args()
+
+
+# An input function that can prefill in the text entry
+def rlinput(prompt, prefill=''):
+    readline.set_startup_hook(lambda: readline.insert_text(prefill))
+    try:
+        # This was raw_input, but that's deprecated in py3
+        return input(prompt)
+    finally:
+        readline.set_startup_hook()
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def run_plumed_sum_hills():
+    """This function takes no arguments and returns nothing.
+    Using the command line values stored in args, it will run the PLUMED
+    sum_hills utility."""
+    command_line = ['mpirun', '-n', '1', 'plumed', 'sum_hills']
+    command_line += ['--hills', args.hills]
+    if args.verbose:
+        print('hills file name is {}'.format(args.hills))
+    command_line += ['--stride', str(args.stride)]
+    if args.verbose:
+        print('data output stride is {}'.format(args.stride))
+    minim = args.min
+    maxim = args.max
+    # todo add option for spacing instead of bin
+    if args.bin:
+        command_line += ['--bin', str(args.bin)]
+        if args.verbose:
+            print('number of bins is {}'.format(args.bin))
+        if minim or maxim:
+            if not (minim and maxim and args.bin):
+                print('If you give a min or max, you need min, '
+                      'max, and bin')
+                minim = rlinput('min = ', minim)
+                maxim = rlinput('max = ', maxim)
+            if args.verbose:
+                print('min: {}, max: {}'.format(minim, maxim))
+            command_line += ['--min', str(minim), '--max', str(maxim)]
+    else:
+        if minim or maxim:
+            print('If you give a min or max, you need min, '
+                  'max, and bin')
+            command_line += ['--bin', str(rlinput('bin = '))]
+            if not (minim and maxim):
+                minim = rlinput('min = ', minim)
+                maxim = rlinput('max = ', maxim)
+            if args.verbose:
+                print('min: {}, max: {}'.format(minim, maxim))
+            command_line += ['--min', str(minim), '--max', str(maxim)]
+    command_line_str = ' '.join(command_line)
+    print('command line argument is:\n{}'.format(command_line_str))
+    print('Running PLUMED sum_hills utility...')
+    # Run the PLUMED sum_hills utility and save/print the output as it comes
+    with open('plumed_output.log', 'w') as log_file:
+        with subprocess.Popen(command_line,
+                              stdout=subprocess.PIPE, bufsize=1,
+                              universal_newlines=True) as proc:
+            for line in proc.stdout:
+                log_file.write(line)
+                if args.verbose:
+                    print(line, end='')
+    print('Done running PLUMED sum_hills utility')
+
+
+def setup_folder():
+    """This function takes no arguments and returns nothing.
+    It will create a folder for making all this data, and
+    it will copy the specified hills file there."""
+    working_folder = args.folder
+    # Make the working folder, don't raise error if it already exists
+    os.makedirs(working_folder, exist_ok=True)
+    copyout = shutil.copy(args.hills, working_folder)
+    if args.verbose:
+        print('HILLS file copied to {}'.format(copyout))
+    os.chdir(working_folder)
+
+
+def read_plumed_stuff():
+    """This function takes no arguments and returns nothing.
+    It will save the output data from plumed to a formatted
+    temporary file that can then be read to put the data
+    into the file that Mathematica can read.
+    If defines the global variable num_of_cvs."""
+    global num_of_cvs, formatted_data
+    print('Reading PLUMED output files')
+    file_names = glob.glob('fes*.dat')
+    # Make sure the list is in the proper order:
+    file_names.sort()         # sort files alphanumerically
+    file_names.sort(key=len)  # sort files by length
+    # Find number of CVs:
+    #  At least in the current implementation of PLUMED, the output is
+    #  the list of CV coordinates, then the height there, then the
+    #  derivative with respect to each of the CVs, hence the
+    #  (number of fields - 1) / 2.
+    with open(file_names[0], 'r') as file:
+        for line in file:
+            if line.startswith('#'):
+                continue
+            num_fields = len(line.split())
+            num_of_cvs = (num_fields - 1) / 2
+            if num_of_cvs.is_integer():
+                num_of_cvs = int(num_of_cvs)
+            else:
+                print('number of CVs found to be {}!'.format(num_of_cvs))
+                num_of_cvs = rlinput('Real number of CVs = ', int(num_of_cvs))
+            break
+    all_data = []
+    for file in file_names:
+        f_data = []
+        if args.verbose:
+            print('Reading file {}'.format(file))
+        with open(file, 'r') as crf:
+            l_data = []
+            for line in crf:
+                if line.startswith('#'):
+                    continue
+                try:
+                    if is_number(line.split()[0]):
+                        l_data += [', '.join(line.split()[0:(num_of_cvs+1)])]
+                except IndexError:
+                    # Blank lines in files have length 0
+                    continue
+            f_data += ['},\n{'.join(l_data)]
+        all_data += f_data
+    formatted_data = '{{{' + '}},\n{{'.join(all_data) + '}}}\n\n'
+    with open(args.temp_file, 'w') as tf:
+        tf.write(formatted_data)
+    print('Done reading PLUMED output data')
+
+
+def data_into_mfile():
+    """This function takes no arguments and returns nothing.
+    It will take data saved to a temporary file from read_plumed_stuff
+    and put it into the template .m file so that it can be read into
+    Mathematica"""
+    print('Putting data into output file...')
+    about_content = []
+    about_content += ['Number of CVs: {}'.format(num_of_cvs)]
+    about_content += ['Number of points per time chunk: '
+                      '{}'.format(args.stride)]
+    about_content += ['Originally processed on {}'.format(datetime.now())]
+    about_content += ['Processed with {}'.format(os.path.basename(__file__))]
+    replacements = dict(varname=args.var_name, data=formatted_data,
+                        numcvs=num_of_cvs, stride=args.stride)
+    if args.spacing:
+        # todo change this when spacing option implemented
+        about_content += ['Grid spacing (unused): {}'.format(args.spacing)]
+        replacements['spacing'] = args.spacing
+    else:
+        replacements['spacing'] = '(Print["getGridSize not currently' \
+                                  'defined"]; $Failed'
+    about = '{' + ', '.join(about_content) + '}'
+    replacements['about'] = about
+    print(replacements.keys())
+    with open(args.template, 'r') as template, \
+            open(args.output_name, 'w') as output:
+        for line in template:
+            if line.startswith('#'):
+                # Remove the '#'
+                line = line[1:]
+                try:
+                    # Unpack the dict, then do the replacements
+                    output.write(line.format(**replacements))
+                except KeyError as e:
+                    print('Error! Key {} not found!'.format(e))
+                    choice = input('(a)bort or (s)kip? ')
+                    if choice in 'abort':
+                        raise e
+                    if choice in 'skip':
+                        continue
+            else:
+                output.write(line)
+    print('Output saved as {}'.format(args.output_name))
+
+
+def clean_up():
+    """This function takes no arguments and returns nothing.
+    It will ask if the temp data should be deleted, and if so, will clean
+    it all up. It can also move the output file back to the original
+    directory.
+    !! put in info about defaults here !!
+    """
+    # print('Cleaning up...')
+    pass
+# todo decide on defaults for cleaning up
+# todo add arguments for defaults for cleaning up
+
+
+if not args.exists:
+    setup_folder()
+    run_plumed_sum_hills()
+read_plumed_stuff()
+data_into_mfile()
+print('Done!')
